71d0a963c6b46e60c6b56b7d112ba0a2b79abb96
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-05-23 05:41:31 -0700

    Add HeroUI to project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import {heroui} from "@heroui/react";
 
 export default {
   content: [
@@ -14,5 +15,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [heroui()],
 } satisfies Config;
