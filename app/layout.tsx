ea12c63d3bdec40e8db441d7e44de3ac1d18793c
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-05-24 06:56:33 -0700

    Add react-hot-toast to repo for user feedback

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { ClerkProvider } from '@clerk/nextjs';
-import { HeroUIProvider } from '@heroui/react';
+import { HeroUIProvider } from '@heroui/react'
+import { Toaster } from 'react-hot-toast';;
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 
@@ -31,6 +32,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <Toaster />
           <HeroUIProvider>{children}</HeroUIProvider>
         </body>
       </html>
--- app/onboard/index.tsx
+++ app/onboard/index.tsx
@@ -1,12 +1,18 @@
 'use client';
 import { ReactNode, useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import toast from 'react-hot-toast';
 import {Spinner} from "@heroui/react";
 import { IUser } from '@/utils/mongodb';
 import { registerNewUser } from '@/actions/onboard';
 
+const successMessage = "Yay! 🥳 You've been registered to AlwaysSaved. Welcome board! 👋🏼";
+const errorMessage = "Whoops! 😱 Looks like there was a problem registering you to AlwaysSaved. 🤦🏽 Try again later.";
+const duration = { duration: 5000 };
 
 export default function Onboard(): ReactNode {
 
+  const router = useRouter();  
   const [onboardedUser, setOnboardedUser] = useState<IUser | null>(null)
 
   useEffect(() => {
@@ -15,14 +21,18 @@ export default function Onboard(): ReactNode {
 
         if(newUser) {
             setOnboardedUser(newUser);
+            toast.success(successMessage, duration);
+            router.push('/home');
         }
+
+        toast.error(errorMessage, duration)
 
 
     }
 
     getNewUser();
 
-  }, [])
+  }, [router])
 
 
   return (
