e6a158dd612ca467fc4d230becaabe44535a6f28
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-05-24 11:42:41 -0700

    Add react-dropzone to project for file uploads

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,39 +1,48 @@
 'use client';
-import { useEffect, useState } from "react"
-import { LeanUser } from "@/utils/mongodb"
+import { useEffect, useState } from 'react';
+import Dropzone from 'react-dropzone';
+import { LeanUser } from '@/utils/mongodb';
 import { getUserFromDB } from '@/actions';
 
-const errorMessage = "Looks like you might have never registered to AlwaysSaved. 😬 Try again later.";
+const errorMessage =
+  'Looks like you might have never registered to AlwaysSaved. 😬 Try again later.';
 
 export default function HomePage() {
+  const [currentUser, setCurrentUser] = useState<LeanUser | null>(null);
 
-    const [currentUser, setCurrentUser] = useState<LeanUser | null>(null);
+  console.log('currentUser ', currentUser);
 
-    console.log("currentUser ", currentUser);
+  useEffect(() => {
+    async function loadCurrentUser() {
+      const currentUser = await getUserFromDB();
 
-    useEffect(() => {
-        async function loadCurrentUser() {
-            const currentUser = await getUserFromDB()
-            
-                    if(currentUser) {
-                        setCurrentUser(currentUser);
-                        return;
-                    }
-            
-                    throw new Error(errorMessage);
+      if (currentUser) {
+        setCurrentUser(currentUser);
+        return;
+      }
 
-        }
+      throw new Error(errorMessage);
+    }
 
-        loadCurrentUser();
+    loadCurrentUser();
+  }, []);
+  return (
+    <div className="flex">
+      <div className="w-[15%] bg-[#f9f9f9] min-h-screen"></div>
 
-    }, [])
-    return (<div className="flex">
-        <div className="w-[15%] bg-[#f9f9f9] min-h-screen">
-
-        </div>
-
-        <main className="p-6">
-          <h1>🏡 Home Page</h1>
-        </main>
-    </div>)
+      <main className="p-6">
+        <h1>🏡 Home Page</h1>
+        <Dropzone onDrop={(acceptedFiles) => console.log(acceptedFiles)}>
+          {({ getRootProps, getInputProps }) => (
+            <section className="border-4 border-dashed p-10">
+              <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <p>Drag and drop some files here, or click to select files</p>
+              </div>
+            </section>
+          )}
+        </Dropzone>
+      </main>
+    </div>
+  );
 }
