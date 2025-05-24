a8df68b443bc0c2bbbb43601cf90328413a24f55
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-05-24 06:43:34 -0700

    Add registerNewUser Server Action

---
+++ actions/onboard/index.ts
@@ -0,0 +1,48 @@
+'use server';
+import { dbConnect, IUser } from '@/utils/mongodb';
+import { UserModel } from '@/utils/mongodb';
+import { currentUser } from '@clerk/nextjs/server';
+
+export const registerNewUser = async (): Promise<IUser | void> => {
+  try {
+    await dbConnect();
+    const clerkUser = await currentUser();
+
+    if(clerkUser === null) {
+
+        throw new Error("There was a problem registering the new user to the app.")
+
+    }
+
+    const first_name = clerkUser?.firstName || '';
+
+    const last_name = clerkUser?.lastName || '';
+
+    const email = clerkUser.emailAddresses[0].emailAddress;
+
+    const avatar_url = clerkUser?.imageUrl || null;
+
+    const payload = {
+        first_name,
+        last_name,
+        email,
+        avatar_url
+    };
+
+
+
+    const [newUser] = await UserModel.create([payload], {j: true});
+
+    console.log("newUser in onboardUser ", newUser);
+    console.log("\n");
+
+    return newUser;
+
+    
+  } catch (error) {
+    // TODO: Handle in Telemetry
+    console.log('Error in registerNewUser: ', error);
+  }
+
+  return undefined;
+};
