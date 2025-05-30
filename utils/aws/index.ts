066d2d63a44df870f7d3e0ef9f4905912db8117e
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-05-30 15:23:39 -0700

    Move getAWSConfigByEnv to utils/aws/index.ts

---
+++ utils/aws/index.ts
@@ -0,0 +1,34 @@
+type AWSClientConfig = {
+  region: string;
+  credentials?: {
+    accessKeyId: string;
+    secretAccessKey: string;
+  };
+};
+
+export const getAWSConfigByEnv = (nodeEnv: string): AWSClientConfig => {
+
+  const region = process.env.AWS_REGION || 'us-east-1';  
+
+  if (nodeEnv === 'development') {
+
+    const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+    const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+
+    if(!accessKeyId || !secretAccessKey) {
+      throw new Error("Missing environment variables in development to make s3 file upload. Cannot continue.")
+    }
+
+    return {
+      region,
+      credentials: { accessKeyId, secretAccessKey }
+    };
+
+
+  }
+
+
+  return {
+    region
+  };
+};
