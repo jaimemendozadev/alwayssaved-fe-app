fde20531d0d9e80c307f98a2473ad6fed80da550
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-06-06 10:44:00 -0700

    Move handleUpload to new FileUploadContext
    Need to create a FileUploadContext that sits
    at the very top of the app and handles uploading
    files & rendering user feedback regardless if
    user stays on page where <FileUpload />
    component is rendered, or goes to another page
    in the app.

---
+++ utils/context/fileuploadcontext.tsx
@@ -0,0 +1,150 @@
+'use client';
+import { useState, ReactNode, createContext, useEffect } from 'react';
+import toast from 'react-hot-toast';
+import { getUserFromDB } from '@/actions';
+import {
+  handleFileDeletion,
+  handleNoteDeletion,
+  handlePresignedUrls,
+} from '@/actions/fileupload';
+import { sendSQSMessage } from '@/utils/aws';
+import { LeanUser } from '@/utils/mongodb';
+
+
+const errorMessage =
+  'Looks like you might have never registered to AlwaysSaved. 😬 Try again later.';
+
+interface FileUploadContext {
+  inFlight: boolean;
+}
+
+export const FileUploadContext = createContext({});
+
+export const FileUploadProvider = ({
+  children
+}: {
+  children: ReactNode;
+}): ReactNode => {
+
+  const [currentUser, setCurrentUser] = useState<LeanUser | null>(null);
+
+  useEffect(() => {
+      async function loadCurrentUser() {
+        const currentUser = await getUserFromDB();
+  
+        if (currentUser) {
+          setCurrentUser(currentUser);
+          return;
+        }
+  
+        throw new Error(errorMessage);
+      }
+  
+      loadCurrentUser();
+    }, []);
+
+
+  const handleUpload = async <T extends File>(acceptedFiles: T[]) => {
+
+    if(!currentUser) return;
+    
+    const currentUserID = currentUser._id;
+
+    // 1) Create a Note document.
+    const createdNote = await createNoteDocument(currentUserID, noteTitle);
+
+    if (!createdNote) {
+      toast.error(basicErrorMsg, feedbackDuration);
+      return;
+    }
+
+    // 2) Create all the File documents associated with that Note.
+    let currentFiles = [...acceptedFiles];
+
+    const fileInfoArray = currentFiles.map((file) => ({
+      name: file.name,
+      type: file.type
+    }));
+
+    const createdFiles = await createFileDocuments(
+      fileInfoArray,
+      currentUserID,
+      createdNote._id
+    );
+
+    if (createdFiles.length === 0) {
+      await handleNoteDeletion(createdNote);
+
+      toast.error(basicErrorMsg, feedbackDuration);
+      return;
+    }
+
+    // 2a) If some of the File documents failed to be created, filter the acceptedFiles.
+    if (createdFiles.length !== currentFiles.length) {
+      currentFiles = filterCurrentFiles(currentFiles, createdFiles);
+      toast.error(
+        'There was a problem uploading some of your files, try again later.',
+        feedbackDuration
+      );
+    }
+
+    // 3) Create the presignUrls for each File document.
+    const presignPayloads = await handlePresignedUrls(createdFiles);
+
+    // 3a) If some or all of the presignURLs failed to be created, take the appropriate steps.
+    if (presignPayloads.length === 0) {
+      await handleNoteDeletion(createdNote);
+
+      if (createdFiles.length > 0) {
+        const fileIDs = createdFiles.map((file) => file._id);
+        await handleFileDeletion(fileIDs);
+      }
+
+      toast.error(basicErrorMsg, feedbackDuration);
+      return;
+    }
+
+    if (presignPayloads.length !== currentFiles.length) {
+      currentFiles = filterCurrentFiles(currentFiles, presignPayloads);
+      toast.error(
+        'There was a problem uploading some of your files, try again later.',
+        feedbackDuration
+      );
+    }
+
+    // 4) Upload each media file to s3.
+    const uploadResults = await handleS3FileUploads(
+      currentFiles,
+      presignPayloads
+    );
+
+    /*
+      5) Verify media uploads were successful, perform database updates to each 
+         File document with their s3_key, prep sqsPayload for sending SQS message.
+    */
+    const feedback = await verifyProcessUploadResults(
+      uploadResults,
+      presignPayloads,
+      createdNote
+    );
+
+    if (feedback.error) {
+      toast.error(feedback.message, feedbackDuration);
+      return;
+    }
+
+    toast.success(feedback.message, feedbackDuration);
+
+    const sqs_message = {
+      user_id: currentUserID,
+      media_uploads: feedback.sqsPayload
+    };
+
+    // 6) Send SQS Message to EXTRACTOR_PUSH_QUEUE to Kick-Off ML Pipeline.
+    await sendSQSMessage(sqs_message);
+  };
+
+  return (
+    <FileUploadContext.Provider value={}>{children}</FileUploadContext.Provider>
+  );
+};
