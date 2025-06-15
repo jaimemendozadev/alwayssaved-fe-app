201c1e4f0d826d72f2e8eb9289d8fe58399e3c7d
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-06-15 08:19:35 -0700

    Add BackendResponse interface to /ts folder

--- utils/ts/index.ts
+++ utils/ts/index.ts
@@ -1,5 +1,13 @@
 import { ChangeEvent, SyntheticEvent } from 'react';
 
+export type HTTP_METHODS = 'POST' | 'PUT' | 'DELETE' | 'GET' | 'PATCH';
+
 export type InputEvent = ChangeEvent<HTMLInputElement>;
 
 export type SubmitEvent = SyntheticEvent;
+
+export interface BackendResponse<T> {
+  status?: number;
+  payload?: T;
+  message?: string;
+}
