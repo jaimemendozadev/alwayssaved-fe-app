b8c034f4db1c1f8bfed106ccc7997d024af8bba7
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-06-16 06:15:27 -0700

    Add delete s3 and database file logic in sqs message try/catch

--- utils/context/filteruploadcontext/utils/uploadManager.ts
+++ utils/context/filteruploadcontext/utils/uploadManager.ts
@@ -16,6 +16,7 @@ await handleFileDeletion(fileIDs);
 class UploadManager {
   uploads = new Map();
 
+  // See Dev Notes below.
   async uploadFile<T extends File>(
     file: T,
     targetPayload: presignPayload,
@@ -33,11 +34,10 @@ class UploadManager {
       send_sqs_message_status: 'failure'
     };
 
-    /*
-       1) Upload file to s3.
-         - Return early if s3 upload fails.
+    const s3DeleteURL = `/api/s3/${s3_key}`;
+    const fileDeleteURL = `/api/files/${file_id}`;
 
-    */
+    // 1) Upload file to s3.
     try {
       await fetch(presigned_url, {
         method: 'PUT',
@@ -59,10 +59,7 @@ class UploadManager {
       return uploadStatus;
     }
 
-    /*
-       2) Make /api request to update File document s3_key property.
-         - Delete the uploaded file in s3 bucket and File document in database if File document update fails.
-    */
+    // 2) Make /api request to update File document s3_key property.
     try {
       const updateResponse: BackendResponse<unknown> = await fetch(
         '/api/files',
@@ -87,13 +84,9 @@ class UploadManager {
         `Error in UploadManager.uploadFile updating File ${file_id} in database: ${message}`
       );
 
-      const s3DeleteURL = `/api/s3/${s3_key}`;
-
       await fetch(s3DeleteURL, {
         method: 'DELETE'
       });
-
-      const fileDeleteURL = `/api/files/${file_id}`;
 
       await fetch(fileDeleteURL, {
         method: 'DELETE'
@@ -126,6 +119,16 @@ class UploadManager {
       console.log(
         `Error in UploadManager.uploadFile sending Extractor Queue Message for s3_key ${s3_key}: ${message}`
       );
+
+      await fetch(s3DeleteURL, {
+        method: 'DELETE'
+      });
+
+      await fetch(fileDeleteURL, {
+        method: 'DELETE'
+      });
+
+      return uploadStatus;
     }
 
     return uploadStatus;
@@ -138,3 +141,20 @@ class UploadManager {
 }
 
 export const uploadManager = new UploadManager();
+
+/***************************
+  Notes
+ ***************************
+
+ 1) uploadFile has 3 steps where we:
+      - Upload the file to s3;
+      - Update the File document with the uploaded s3_key; and
+      - Send a Message to the SQS Extractor Queue.
+
+    If the upload to s3 fails we return early, or if either the db
+    update or sending the SQS message fails, we delete the uploaded
+    s3 file and the file document in the db. In v1, it's an all or
+    nothing operation and we don't want dangling files in s3 or
+    in the database.
+
+*/
