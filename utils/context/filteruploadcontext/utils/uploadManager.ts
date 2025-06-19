71aeff83b30503adc72df54f2d3ff57d9c4d8851
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-06-19 05:20:13 -0700

    Add processFile.ts file
    Turns out uploadManager does not enable inflight upload
    requests to continue when user navigates to another page.
    Need to fix this later.

--- utils/context/filteruploadcontext/utils/uploadManager.ts
+++ utils/context/filteruploadcontext/utils/uploadManager.ts
@@ -3,7 +3,7 @@ import { BackendResponse } from '@/utils/ts';
 
 export interface ProcessStatus {
   s3_key: string;
-  process_status: 'success';
+  process_status: 'success' | 'failure';
 }
 class UploadManager {
   uploads = new Map();
@@ -12,9 +12,6 @@ class UploadManager {
   async processFile<T extends File>(
     file: T,
     targetPayload: presignPayload,
-    onProgress,
-    onDone,
-    onError
   ): Promise<ProcessStatus> {
     const controller = new AbortController();
 
@@ -22,13 +19,18 @@ class UploadManager {
 
     const processStatus: ProcessStatus = {
       s3_key,
-      process_status: 'success'
+      process_status: 'failure'
     };
 
     const s3DeleteURL = `/api/s3/${s3_key}`;
     const fileDeleteURL = `/api/files/${file_id}`;
 
     try {
+      console.log(`Start processing for ${s3_key}.`);   
+      console.log(`Performing Step 1) Upload file to s3 for s3_key .`);  
+      console.log("\n");
+
+
       // 1) Upload file to s3.
       const s3UploadRes = await fetch(presigned_url, {
         method: 'PUT',
@@ -37,7 +39,8 @@ class UploadManager {
         signal: controller.signal
       });
 
-      console.log('s3UploadRes \n ', s3UploadRes);
+      console.log('s3UploadRes in Step #1 \n', s3UploadRes);
+      console.log("\n");
 
       if (s3UploadRes.status !== 200) {
         throw new Error(
@@ -45,10 +48,13 @@ class UploadManager {
         );
       }
 
-      console.log('Step #1 s3 upload successful. \n');
+      console.log('Step #1 s3 upload successful.');
+      console.log("\n");
 
       this.uploads.set(file.name, controller);
 
+      console.log("Performing Step 2) Make /api request to update File document");
+      console.log("\n");
       // 2) Make /api request to update File document s3_key property.
       const updateResponse: BackendResponse<unknown> = await fetch(
         '/api/files',
@@ -58,6 +64,9 @@ class UploadManager {
           body: JSON.stringify({ file_id, update: { s3_key } })
         }
       );
+
+      console.log("updateResponse for Step 2) Make /api request to update File document", updateResponse);
+      console.log("\n");
 
       if (updateResponse.status !== 200) {
         await fetch(s3DeleteURL, {
@@ -73,9 +82,11 @@ class UploadManager {
         );
       }
 
-      console.log('Step #2 File database update successful. \n');
+      console.log('Step #2 File database update successful.');
+      console.log("\n");
 
-      console.log('updateResponse \n', updateResponse);
+      console.log("Performing Step 3) Send SQS Message in Backend.");
+      console.log("\n");
 
       // 3) Send SQS Message in Backend.
       const sqsResponse: BackendResponse<unknown> = await fetch(
@@ -86,6 +97,10 @@ class UploadManager {
           body: JSON.stringify({ note_id, user_id, s3_key })
         }
       );
+
+      console.log("sqsResponse for Step 3 ", sqsResponse);
+      console.log(`End processing for ${s3_key}.`);  
+      console.log("\n");
 
       if (sqsResponse.status !== 200) {
         await fetch(s3DeleteURL, {
@@ -100,6 +115,12 @@ class UploadManager {
           `There was a problem sending a message to the Extractor Queue for user ${user_id} with s3_key ${s3_key}.`
         );
       }
+
+
+      console.log("Step #3 Send SQS Message in Backend was a success");
+      console.log("\n");
+
+
     } catch (error) {
       const message = error instanceof Error ? error.message : String(error);
 
@@ -107,6 +128,8 @@ class UploadManager {
         `Error in UploadManager.uploadFile uploading file with s3_key: ${s3_key}: ${message}`
       );
     }
+
+    processStatus['process_status'] = 'success';
 
     return processStatus;
   }
@@ -119,11 +142,20 @@ class UploadManager {
 
 export const uploadManager = new UploadManager();
 
-/***************************
-  Notes
- ***************************
 
- 1) uploadFile has 3 steps where we:
+/********************************************
+ * Notes
+ ********************************************
+
+ 1) Media assets are stored in s3 in the following s3 Key format: 
+
+    /{fileOwner}/{noteID}/{fileID}/{fileName}.{fileExtension} 
+
+    fileOwner: is the User._id
+    fileName: is the name of the file with the fileType extension 
+
+
+2) uploadFile has 3 steps where we:
       - Upload the file to s3;
       - Update the File document with the uploaded s3_key; and
       - Send a Message to the SQS Extractor Queue.
@@ -133,5 +165,4 @@ export const uploadManager = new UploadManager();
     s3 file and the file document in the db. In v1, it's an all or
     nothing operation and we don't want dangling files in s3 or
     in the database.
-
 */
