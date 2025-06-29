98621e2a7546f4f332bbf2ea46f27b8cd32720be
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-06-29 07:48:50 -0700

    Fix left side navbar in page layouts

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,5 +1,4 @@
 'use server';
-import { getNotesForSideNav } from '@/actions/layout';
 import { ReactNode } from 'react';
 import Link from 'next/link';
 
@@ -8,17 +7,11 @@ export default async function CommonPageLayout({
 }: {
   children: ReactNode;
 }): Promise<ReactNode> {
-  const userNotes = await getNotesForSideNav();
 
   return (
     <div className="flex">
-      <div className="w-[15%] bg-[#f9f9f9] min-h-screen p-4">
-        <p className="text-[#919191] mb-2">Notes</p>
-        {userNotes.map((noteDoc) => (
-          <Link key={noteDoc._id} href={`/notes/${noteDoc._id}`}>
-            {noteDoc.title}
-          </Link>
-        ))}
+      <div className="w-[15%] bg-[#f9f9f9] min-h-screen p-5">
+        <p className="text-[#919191] font-semibold mb-2"><Link href="/notes">📝 Notes</Link></p>
       </div>
 
       {children}
