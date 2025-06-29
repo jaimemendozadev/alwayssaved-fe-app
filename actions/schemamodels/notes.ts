612e1cfe2efa5ba399aed2e275cd6eab490fef5a
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-06-29 07:34:46 -0700

    Add new getNotesByFields schemamodels Server Action

--- actions/schemamodels/notes.ts
+++ actions/schemamodels/notes.ts
@@ -1,18 +1,39 @@
 'use server';
-import { deepLean, getObjectIDFromString, LeanNote, NoteModel } from "@/utils/mongodb";
+import {
+  deepLean,
+  getObjectIDFromString,
+  LeanNote,
+  NoteModel
+} from '@/utils/mongodb';
 
+export const getNoteByID = async (noteID: string): Promise<LeanNote | void> => {
+  const mongoID = getObjectIDFromString(noteID);
+  const foundNote = await NoteModel.findById(mongoID).exec();
 
-export const getNoteByID = async(noteID: string): Promise<LeanNote | void> => {
-    const mongoID = getObjectIDFromString(noteID);
-    const foundNote = await NoteModel.findById(mongoID).exec();
+  if (!foundNote) {
+    throw new Error(`Can't find a note with an ID of ${noteID}`);
+  }
 
+  return deepLean(foundNote);
+};
 
-    if(!foundNote) {
-        throw new Error(`Can't find a note with an ID of ${noteID}`)
-    }
-
-
-    return deepLean(foundNote);
-
-
+interface SpecifiedNoteFields {
+  _id?: number;
+  user_id?: number;
+  title?: number;
+  date_created?: number;
+  date_deleted?: number;
+  files?: number;
 }
+
+export const getNotesByFields = async (
+  userID: string,
+  docFields: SpecifiedNoteFields
+): Promise<LeanNote[]> => {
+  const foundNotes = await NoteModel.aggregate([
+    { $match: { user_id: getObjectIDFromString(userID) } },
+    { $project: docFields }
+  ]);
+
+  return deepLean(foundNotes);
+};
--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -2,30 +2,40 @@
 import { useEffect, useState, ReactNode } from 'react';
 import { getUserFromDB } from '@/actions';
 import { LeanUser, LeanNote } from '@/utils/mongodb';
-export default function NotePage():ReactNode {
+import { getNotesByFields } from '@/actions/schemamodels/notes';
+
+export default function NotesPage(): ReactNode {
   const [currentUser, setCurrentUser] = useState<LeanUser | null>(null);
   const [appNotes, setAppNotes] = useState<LeanNote[] | null>(null);
 
-
+  console.log('currentUser in NotePage ', currentUser);
+  console.log('appNotes in NotesPage ', appNotes);
+  console.log('\n');
 
   useEffect(() => {
-    async function loadCurrentUser() {
+    async function loadPageData() {
       const currentUser = await getUserFromDB();
 
       if (currentUser) {
         setCurrentUser(currentUser);
+
+        const userNotes = await getNotesByFields(currentUser._id, {
+          _id: 0,
+          title: 0,
+          date_created: 0
+        });
+
+        setAppNotes(userNotes);
         return;
       }
     }
 
-    loadCurrentUser();
+    loadPageData();
   }, []);
 
   return (
     <main className="p-6 w-[85%]">
-      <h1 className="text-3xl lg:text-6xl mb-16">
-        Your App Notes
-      </h1>
+      <h1 className="text-3xl lg:text-6xl mb-16">Your App Notes</h1>
     </main>
   );
 }
