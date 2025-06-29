e124b621b9cc0ff6a5c615d25e06fdc4d7a2f917
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-06-29 07:03:13 -0700

    Render Notes links in layout left nav sidebar

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,5 +1,6 @@
 import { getNotesForSideNav } from '@/actions/layout';
 import { ReactNode } from 'react';
+import Link from 'next/link';
 
 export default async function HomePageLayout({
   children
@@ -11,7 +12,12 @@ export default async function HomePageLayout({
   return (
     <div className="flex">
       <div className="w-[15%] bg-[#f9f9f9] min-h-screen p-4">
-        <p className="text-[#919191]">Notes</p>
+        <p className="text-[#919191] mb-2">Notes</p>
+        {userNotes.map((noteDoc) => (
+          <Link key={noteDoc._id} href={`/notes/${noteDoc._id}`}>
+            {noteDoc.title}
+          </Link>
+        ))}
       </div>
 
       {children}
