4d9931f35fd841b4fb2673e880b1e27a8430c40e
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-07-04 08:36:46 -0700

    Add new [noteID]/edit Page
    Also start refactoring /notes/[noteID] Page.

---
+++ app/notes/[noteID]/edit/page.tsx
@@ -0,0 +1,42 @@
+'use server';
+import { ReactNode } from 'react';
+import { getUserFromDB } from '@/actions';
+import { NumberNoteMainUI } from '@/components/[noteID]';
+import { getNoteByID } from '@/actions/schemamodels/notes';
+import { getFilesByFields } from '@/actions/schemamodels/files';
+
+export default async function NotePage({
+  params
+}: {
+  params: { noteID: string };
+}): Promise<ReactNode> {
+  const { noteID } = await params;
+
+  console.log('noteID in [noteID]/edit ', noteID);
+
+  const currentNote = await getNoteByID(noteID);
+
+  const currentUser = await getUserFromDB();
+
+  let files = null;
+
+  if (currentNote && currentUser) {
+    files = await getFilesByFields(currentUser._id, {
+      _id: 1,
+      s3_key: 1,
+      file_name: 1,
+      file_type: 1
+    });
+  }
+
+  if (currentUser && currentNote && files) {
+    return (
+      <NumberNoteMainUI
+        currentUser={currentUser}
+        currentNote={currentNote}
+        noteFiles={files}
+      />
+    );
+  }
+  throw new Error(`There was an error displaying the Note Page for ${noteID}`);
+}
--- app/notes/[noteID]/page.tsx
+++ app/notes/[noteID]/page.tsx
@@ -1,11 +1,10 @@
 'use server';
 import { ReactNode } from 'react';
+import Link from 'next/link';
 import { getUserFromDB } from '@/actions';
-import { NumberNoteMainUI } from '@/components/[noteID]';
 import { getNoteByID } from '@/actions/schemamodels/notes';
 import { getFilesByFields } from '@/actions/schemamodels/files';
-
-export default async function NotePage({
+export default async function NoteIDPage({
   params
 }: {
   params: { noteID: string };
@@ -16,10 +15,10 @@ export default async function NotePage({
 
   const currentUser = await getUserFromDB();
 
-  let files = null;
+  let noteFiles = null;
 
   if (currentNote && currentUser) {
-    files = await getFilesByFields(currentUser._id, {
+    noteFiles = await getFilesByFields(currentUser._id, {
       _id: 1,
       s3_key: 1,
       file_name: 1,
@@ -27,13 +26,35 @@ export default async function NotePage({
     });
   }
 
-  if (currentUser && currentNote && files) {
+  if (currentUser && currentNote && noteFiles) {
     return (
-      <NumberNoteMainUI
-        currentUser={currentUser}
-        currentNote={currentNote}
-        noteFiles={files}
-      />
+      <main className="p-6 w-[85%]">
+        <h1 className="text-3xl lg:text-6xl mb-16">
+          Note Page for: {currentNote?.title}
+        </h1>
+
+        <p className="mb-8">
+          <Link
+            className="hover:underline underline-offset-4"
+            href={`${currentNote._id.toString()}/edit`}
+          >
+            ✍🏼 Edit Your Note
+          </Link>
+        </p>
+
+        {noteFiles.length > 0 && (
+          <ul className="space-y-4 mb-20">
+            {noteFiles.map((fileDoc) => (
+              <li key={fileDoc._id} className="border p-5">
+                <span className="font-semibold">File Name</span>:{' '}
+                {fileDoc.file_name} &nbsp; | &nbsp;{' '}
+                <span className="font-semibold">File Type</span>:{' '}
+                {fileDoc.file_type}
+              </li>
+            ))}
+          </ul>
+        )}
+      </main>
     );
   }
   throw new Error(`There was an error displaying the Note Page for ${noteID}`);
