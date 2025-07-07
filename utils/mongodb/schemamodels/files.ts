18cd8b89d5a55c9c2d86f6e3bc68119dcef64397
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-07-07 09:48:21 -0700

    Update File Schema Model
    When creating a new File, file_type is required, added a
    default value. Need to fix this again later.

--- utils/mongodb/schemamodels/files.ts
+++ utils/mongodb/schemamodels/files.ts
@@ -7,35 +7,36 @@ import { LeanUser } from './users';
 import { LeanNote } from './notes';
 
 export interface IFile {
-    _id: mongoose.Types.ObjectId;
-    user_id: mongoose.Types.ObjectId | IUser;
-    note_id: mongoose.Types.ObjectId | INote;
-    s3_key: string | null;
-    file_name: string;
-    file_type: string;
-    date_uploaded: Date;
+  _id: mongoose.Types.ObjectId;
+  user_id: mongoose.Types.ObjectId | IUser;
+  note_id: mongoose.Types.ObjectId | INote;
+  s3_key: string | null;
+  file_name: string;
+  file_type: string;
+  date_uploaded: Date;
 }
-
-
 
 export type LeanFile = Omit<IFile, '_id' | 'user_id' | 'note_id'> & {
-    _id: string;
-    user_id: string | LeanUser;
-    note_id: string | LeanNote;
-}
+  _id: string;
+  user_id: string | LeanUser;
+  note_id: string | LeanNote;
+};
 
-
-const {Schema, model} = mongoose;
-
+const { Schema, model } = mongoose;
 
 // TODO: Need to fix FileSchema to maybe throw error if created File doc has no file_type or file_name
 const FileSchema = new Schema<IFile>({
   user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   note_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Note' },
-  s3_key: {type: String, default: null},
-  file_name: {type: String, required: true, default: `Untitled File - ${dayjs().format('dddd, MMMM D, YYYY h:mm A')}`},
-  file_type: {type: String, required: true,},
-  date_uploaded: {type: Date, default: Date.now},
+  s3_key: { type: String, default: null },
+  file_name: {
+    type: String,
+    required: true,
+    default: `Untitled File - ${dayjs().format('dddd, MMMM D, YYYY h:mm A')}`
+  },
+  file_type: { type: String, required: true, default: 'Unknown Type' },
+  date_uploaded: { type: Date, default: Date.now }
 });
 
-export const FileModel = mongoose.models?.File || model<IFile>('File', FileSchema);
+export const FileModel =
+  mongoose.models?.File || model<IFile>('File', FileSchema);
