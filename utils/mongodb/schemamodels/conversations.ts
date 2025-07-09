e4c7560fc6dac82f99e375dc4b3d742f0578575d
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-07-09 15:10:24 -0700

    Add title property to Conversation Schema

--- utils/mongodb/schemamodels/conversations.ts
+++ utils/mongodb/schemamodels/conversations.ts
@@ -8,6 +8,7 @@ export interface IConversation {
   _id: mongoose.Types.ObjectId;
   user_id: mongoose.Types.ObjectId | IUser;
   note_id: mongoose.Types.ObjectId | INote;
+  title: string;
   date_started: Date;
   date_archived: Date | null;
   date_deleted: Date | null;
@@ -24,6 +25,7 @@ const { Schema, model } = mongoose;
 const ConversationSchema = new Schema<IConversation>({
   user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   note_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Note' },
+  title: { type: String, default: `Untitled Conversation - ${new Date()}` },
   date_started: { type: Date, default: Date.now },
   date_archived: { type: Date, default: null },
   date_deleted: { type: Date, default: null }
