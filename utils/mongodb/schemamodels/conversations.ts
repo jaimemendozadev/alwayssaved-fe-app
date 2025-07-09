f9cb1c8831ad2bcb021051bf9fe3a8a708367d65
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-07-09 08:55:03 -0700

    Add note_id reference to Conversation Schema

--- utils/mongodb/schemamodels/conversations.ts
+++ utils/mongodb/schemamodels/conversations.ts
@@ -2,10 +2,12 @@ import mongoose from 'mongoose';
 import { IUser } from './users';
 
 import { LeanUser } from './users';
+import { INote, LeanNote } from './notes';
 
 export interface IConversation {
   _id: mongoose.Types.ObjectId;
   user_id: mongoose.Types.ObjectId | IUser;
+  note_id: mongoose.Types.ObjectId | INote;
   date_started: Date;
   date_archived: Date | null;
   date_deleted: Date | null;
@@ -14,17 +16,19 @@ export interface IConversation {
 export type LeanConversation = Omit<IConversation, '_id' | 'user_id'> & {
   _id: string;
   user_id: string | LeanUser;
+  note_id: string | LeanNote;
 };
 
 const { Schema, model } = mongoose;
 
 const ConversationSchema = new Schema<IConversation>({
   user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  date_started: {type: Date, default: Date.now},
+  note_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Note' },
+  date_started: { type: Date, default: Date.now },
   date_archived: { type: Date, default: null },
   date_deleted: { type: Date, default: null }
 });
 
-
 export const ConversationModel =
-  mongoose.models?.File || model<IConversation>('Conversation', ConversationSchema);
+  mongoose.models?.File ||
+  model<IConversation>('Conversation', ConversationSchema);
