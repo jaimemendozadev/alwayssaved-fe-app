c26f978e2ad6c7143565c10d5e7ed2fe13d072d1
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-07-09 15:57:43 -0700

    Add hide logic to Note Convos page in NumberNoteMainUI
    Had to hide button because if the Note has no attached
    Files, it makes it impossible to have a Chat convo with
    the LLM Service.

--- components/[noteID]/numbernotemainui.tsx
+++ components/[noteID]/numbernotemainui.tsx
@@ -31,16 +31,32 @@ export const NumberNoteMainUI = ({
       <h2 className="text-3xl lg:text-4xl mb-6">Files Attached to Your Note</h2>
 
       {noteFiles.length > 0 ? (
-        <ul className="space-y-4 mb-32">
-          {noteFiles.map((fileDoc) => (
-            <li key={fileDoc._id} className="border p-5">
-              <span className="font-semibold">File Name</span>:{' '}
-              {fileDoc.file_name} &nbsp; | &nbsp;{' '}
-              <span className="font-semibold">File Type</span>:{' '}
-              {fileDoc.file_type}
-            </li>
-          ))}
-        </ul>
+        <>
+          <ul className="space-y-4 mb-32">
+            {noteFiles.map((fileDoc) => (
+              <li key={fileDoc._id} className="border p-5">
+                <span className="font-semibold">File Name</span>:{' '}
+                {fileDoc.file_name} &nbsp; | &nbsp;{' '}
+                <span className="font-semibold">File Type</span>:{' '}
+                {fileDoc.file_type}
+              </li>
+            ))}
+          </ul>
+          <p className="text-2xl mb-16">
+            Click on the Link below to go to the Note&#39;s Conversations Page
+            to start a Chat with the LLM about your Note files. 🤖
+          </p>
+
+          <div className="mb-14">
+            <Button
+              size="md"
+              variant="ghost"
+              onPress={() => router.push(convoURL)}
+            >
+              💬 Note Convos
+            </Button>
+          </div>
+        </>
       ) : (
         <div>
           <p className="text-xl mb-1">
@@ -52,17 +68,6 @@ export const NumberNoteMainUI = ({
           </p>
         </div>
       )}
-
-      <p className="text-2xl mb-16">
-        Click on the Link below to go to the Note&#39;s Conversations Page to
-        start a Chat with the LLM about your Note files. 🤖
-      </p>
-
-      <div className="mb-14">
-        <Button size="md" variant="ghost" onPress={() => router.push(convoURL)}>
-          💬 Note Convos
-        </Button>
-      </div>
     </div>
   );
 };
