7028652105c3e97eea8cc0e946027646b371aa33
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-07-09 05:09:59 -0700

    Add Conversations & ConvoMessage Schemas to app

---
+++ utils/mongodb/schemamodels/conversations.ts
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+import { IUser } from './users';
+
+import { LeanUser } from './users';
+
+export interface IConversation {
+  _id: mongoose.Types.ObjectId;
+  user_id: mongoose.Types.ObjectId | IUser;
+  date_started: Date;
+  date_archived: Date | null;
+  date_deleted: Date | null;
+}
+
+export type LeanConversation = Omit<IConversation, '_id' | 'user_id'> & {
+  _id: string;
+  user_id: string | LeanUser;
+};
+
+const { Schema, model } = mongoose;
+
+const ConversationSchema = new Schema<IConversation>({
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  date_started: {type: Date, default: Date.now},
+  date_archived: { type: Date, default: null },
+  date_deleted: { type: Date, default: null }
+});
+
+
+export const ConversationModel =
+  mongoose.models?.File || model<IConversation>('Conversation', ConversationSchema);
---
+++ utils/mongodb/schemamodels/convomessages.ts
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+import { IUser } from './users';
+
+import { LeanUser } from './users';
+import { IConversation, LeanConversation } from './conversations';
+
+export interface IConvoMessage {
+  _id: mongoose.Types.ObjectId;
+  conversation_id: mongoose.Types.ObjectId | IConversation;
+  user_id?: mongoose.Types.ObjectId | IUser;
+  date_sent: Date;
+  sender_type: string;
+  message: string;
+}
+
+export type LeanConvoMessage = Omit<IConvoMessage, '_id' | 'conversation_id' | 'user_id'> & {
+  _id: string;
+  conversation_id: string | LeanConversation;
+  user_id: string | LeanUser;
+};
+
+const { Schema, model } = mongoose;
+
+const ConvoMessageSchema = new Schema<IConvoMessage>({
+  conversation_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Conversation' },  
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  date_sent: {type: Date, default: Date.now},
+  sender_type: {type: String, required: true},
+  message: {type: String, required: true},
+});
+
+
+export const ConvoMessageModel =
+  mongoose.models?.File || model<IConvoMessage>('ConvoMessage', ConvoMessageSchema);
