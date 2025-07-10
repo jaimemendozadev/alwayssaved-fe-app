382a64d99537fc4f1ea1150e579f2dcca5d8995d
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-07-10 13:38:47 -0700

    Move list of [noteID] convos to /notes/[noteID]/clientui page
    Figured it was easier to render on a single Note page, the
    list of File attached to the Note, the list of Convos
    attached to the Note, and a link to create a new Convo.

--- app/notes/[noteID]/page.tsx
+++ app/notes/[noteID]/page.tsx
@@ -5,6 +5,7 @@ import { getNoteByID } from '@/actions/schemamodels/notes';
 import { matchProjectFiles } from '@/actions/schemamodels/files';
 import { ClientUI } from '@/components/notes/[noteID]';
 import { getObjectIDFromString } from '@/utils/mongodb';
+import { matchProjectConversations } from '@/actions/schemamodels/conversations';
 export default async function NoteIDPage({
   params
 }: {
@@ -16,25 +17,43 @@ export default async function NoteIDPage({
 
   const currentUser = await getUserFromDB();
 
-  let noteFiles = null;
-
-  if (currentNote && currentUser) {
-    noteFiles = await matchProjectFiles(
-      {
-        user_id: getObjectIDFromString(currentUser._id),
-        note_id: getObjectIDFromString(currentNote._id)
-      },
-      {
-        _id: 1,
-        s3_key: 1,
-        file_name: 1,
-        file_type: 1
-      }
+  if (!currentNote || !currentUser) {
+    throw new Error(
+      `There was a problem getting the Note Page for Note ${noteID}`
     );
   }
 
+  const noteFiles = await matchProjectFiles(
+    {
+      user_id: getObjectIDFromString(currentUser._id),
+      note_id: getObjectIDFromString(currentNote._id)
+    },
+    {
+      _id: 1,
+      s3_key: 1,
+      file_name: 1,
+      file_type: 1
+    }
+  );
+
+  const activeConvos = await matchProjectConversations(
+    {
+      user_id: getObjectIDFromString(currentUser._id),
+      note_id: getObjectIDFromString(currentNote._id),
+      date_deleted: { $eq: null },
+      date_archived: { $eq: null }
+    },
+    { _id: 1, user_id: 1, note_id: 1, title: 1, date_started: 1 }
+  );
+
   if (currentNote && noteFiles) {
-    return <ClientUI currentNote={currentNote} noteFiles={noteFiles} />;
+    return (
+      <ClientUI
+        currentNote={currentNote}
+        noteFiles={noteFiles}
+        convos={activeConvos}
+      />
+    );
   }
   throw new Error(`There was an error displaying the Note Page for ${noteID}`);
 }
--- components/notes/[noteID]/clientui.tsx
+++ components/notes/[noteID]/clientui.tsx
@@ -2,20 +2,32 @@
 import { ReactNode } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import { Button } from '@heroui/react';
-import { LeanNote, LeanFile } from '@/utils/mongodb';
+import dayjs from 'dayjs';
+import { Button, Tooltip } from '@heroui/react';
+import { LeanNote, LeanFile, LeanConversation } from '@/utils/mongodb';
 interface ClientUIProps {
   currentNote: LeanNote;
   noteFiles: LeanFile[];
+  convos: LeanConversation[];
 }
 
 export const ClientUI = ({
   currentNote,
-  noteFiles
+  noteFiles,
+  convos
 }: ClientUIProps): ReactNode => {
   const router = useRouter();
   const editURL = `/notes/${currentNote._id.toString()}/edit`;
   const convoURL = `/notes/${currentNote._id.toString()}/convos`;
+
+  const handleConvoDeletion = async (convoID: string): Promise<void> => {
+    /*
+      TODO: On Conversation Deletion:
+       - Delete All ConvoMessages attached to Conversation._id
+       - Delete Conversation.
+
+    */
+  };
 
   return (
     <div className="p-6 w-[85%]">
@@ -23,7 +35,7 @@ export const ClientUI = ({
         Note Page for: {currentNote?.title}
       </h1>
 
-      <div className="mb-14">
+      <div className="mb-20">
         <Button size="md" variant="ghost" onPress={() => router.push(editURL)}>
           ✍🏼 Edit Note
         </Button>
@@ -31,8 +43,18 @@ export const ClientUI = ({
 
       <h2 className="text-3xl lg:text-4xl mb-6">Files Attached to Your Note</h2>
 
-      {noteFiles.length > 0 ? (
-        <>
+      {noteFiles.length === 0 ? (
+        <div className="mb-36">
+          <p className="text-xl mb-1">
+            You have no files attached to this Note. 😔
+          </p>
+          <p className="text-xl mb-20">
+            You can add files to the note by clicking on the 👆🏽 above
+            &ldquo;Edit Your Note&rdquo; link.
+          </p>
+        </div>
+      ) : (
+        <div className="mb-36">
           <ul className="space-y-4 mb-32">
             {noteFiles.map((fileDoc) => (
               <li key={fileDoc._id} className="border p-5">
@@ -43,6 +65,7 @@ export const ClientUI = ({
               </li>
             ))}
           </ul>
+
           <p className="text-2xl mb-4">
             Click on the link to the{' '}
             <Link
@@ -56,20 +79,65 @@ export const ClientUI = ({
           </p>
 
           <p className="text-2xl">Or you could start a new Chat. 🤖</p>
-        </>
-      ) : (
-        <div>
-          <p className="text-xl mb-1">
-            You have no files attached to this Note. 😔
-          </p>
-          <p className="text-xl mb-20">
-            You can add files to the note by clicking on the 👆🏽 above
-            &ldquo;Edit Your Note&rdquo; link.
-          </p>
         </div>
       )}
 
-      {/* TODO: Should we delete http://localhost:3000/notes/[noteID]/convos page and display all the convos on this page? 🤔 */}
+      <h2 className="text-3xl lg:text-4xl mb-6">
+        💬 Conversations for {currentNote.title} Note
+      </h2>
+
+      {convos.length === 0 ? (
+        <>
+          <p className="text-2xl mb-4">
+            You have no Conversations for this Note.
+          </p>
+          <p className="text-2xl">
+            <Link
+              className="hover:underline underline-offset-4 text-blue-700"
+              href={`/notes/${currentNote._id}/convos/new`}
+            >
+              Create a Conversation for this Note
+            </Link>{' '}
+            to start chatting with the LLM. 🤖
+          </p>
+        </>
+      ) : (
+        <>
+          <p className="text-2xl mb-16">
+            Click on the Conversation link to view the Convo Chat thread and
+            continue chatting with the LLM about your Note. 🦾
+          </p>
+          <ul className="space-y-7">
+            {convos.map((convo) => {
+              return (
+                <li className="border p-5" key={convo._id}>
+                  <Link
+                    className="hover:underline underline-offset-4"
+                    href={`/notes/${convo.note_id}/convos/${convo._id}`}
+                  >
+                    <span className="font-semibold">Convo Name</span>:{' '}
+                    {convo.title} &nbsp; | &nbsp;{' '}
+                    <span className="font-semibold">Convo Start Date</span>:{' '}
+                    {dayjs(convo.date_started).format('dddd, MMMM D, YYYY')}
+                  </Link>
+                  &nbsp; | &nbsp;{' '}
+                  <Tooltip content="Delete Convo">
+                    <Button
+                      size="sm"
+                      variant="ghost"
+                      isIconOnly={true}
+                      aria-label="Delete"
+                      onPress={async () => await handleConvoDeletion(convo._id)}
+                    >
+                      🗑️
+                    </Button>
+                  </Tooltip>
+                </li>
+              );
+            })}
+          </ul>
+        </>
+      )}
     </div>
   );
 };
