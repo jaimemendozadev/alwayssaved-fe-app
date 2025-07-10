124c93f6cc99605b749388864f30e862a27cb589
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-07-09 17:04:03 -0700

    Save changes to [noteID]/convo/[convoID] Page

--- app/notes/[noteID]/convo/[convoID]/page.tsx
+++ app/notes/[noteID]/convo/[convoID]/page.tsx
@@ -1,19 +1,41 @@
 'use server';
 import { ReactNode } from 'react';
 import { getUserFromDB } from '@/actions';
-import { getNoteByID } from '@/actions/schemamodels/notes';
 import { matchProjectFiles } from '@/actions/schemamodels/files';
-import { NumberNoteMainUI } from '@/components/[noteID]';
 import { getObjectIDFromString } from '@/utils/mongodb';
 export default async function ConvoIDPage({
   params
 }: {
-  params: { noteID: string };
+  params: { noteID: string; convoID: string };
 }): Promise<ReactNode> {
+  const currentUser = await getUserFromDB();
   const { noteID, convoID } = await params;
 
   console.log('noteID in ConvoIDPage ', noteID);
   console.log('convoID in ConvoIDPage ', convoID);
+
+  if (!currentUser || !noteID || !convoID) {
+    throw new Error(
+      `There was an error getting the Convo Page for Note ${noteID} Convo ${convoID}. Try again later.`
+    );
+  }
+
+  const files = await matchProjectFiles(
+    {
+      user_id: getObjectIDFromString(currentUser._id),
+      note_id: getObjectIDFromString(noteID),
+      date_deleted: { $eq: null }
+    },
+    {
+      _id: 1,
+      s3_key: 1,
+      file_name: 1,
+      file_type: 1,
+      date_deleted: 1
+    }
+  );
+
+  console.log('files in ConvoIDPage ', files);
 
   return <h1>ConvoIDPage</h1>;
 }
