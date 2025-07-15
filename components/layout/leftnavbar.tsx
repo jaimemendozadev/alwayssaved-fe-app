b7c621782b621fb1562c595c4cf3328838da0d4b
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-07-15 16:34:18 -0700

    Fix width of links in LeftNavbar

--- components/layout/leftnavbar.tsx
+++ components/layout/leftnavbar.tsx
@@ -9,26 +9,26 @@ import { SignOutButton } from '@clerk/nextjs';
 export const LeftNavbar = (): ReactNode => {
   return (
     <div className="max-w-1/6 min-h-screen border bg-[#f9f9f9] p-4">
-      <div className="min-w-28 p-5 mb-2">
-        <p className="text-[#919191] font-semibold mb-2">
+      <div className="min-w-28 p-5 mb-2 w-36">
+        <p className="text-[#919191] font-semibold">
           <Link href="/home">🏡 Home</Link>
         </p>
       </div>
 
-      <div className="min-w-28 p-5">
-        <p className="text-[#919191] font-semibold mb-2">
+      <div className="min-w-28 p-5 mb-2 w-36">
+        <p className="text-[#919191] font-semibold">
           <Link href="/notes">📝 Notes</Link>
         </p>
       </div>
 
-      <div className="min-w-28 p-5">
-        <p className="text-[#919191] font-semibold mb-2">
+      <div className="min-w-28 p-5 mb-2 w-36">
+        <p className="text-[#919191] font-semibold">
           <Link href="/convos">💬 Convos</Link>
         </p>
       </div>
 
-      <div className="min-w-28 p-5">
-        <div className="text-[#919191] font-semibold mb-2">
+      <div className="min-w-28 p-5 w-36">
+        <div className="text-[#919191] font-semibold">
           <SignOutButton />
         </div>
       </div>
