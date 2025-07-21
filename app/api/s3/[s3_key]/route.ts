f2be2f4505131eb87ed7087dc709532fe888958e
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-07-21 16:36:21 -0700

    Fix params error in /api/s3/[s3_key] catch block

--- app/api/s3/[s3_key]/route.ts
+++ app/api/s3/[s3_key]/route.ts
@@ -29,13 +29,15 @@ export async function DELETE(
       `Could not delete file with s3_key ${s3_key} from s3 bucket.`
     );
   } catch (error) {
+    const { s3_key } = await params;
+
     // TODO: Handle in telemetry
-    console.log(`Error in DELETE /api/s3/${params.s3_key} `, error);
+    console.log(`Error in DELETE /api/s3/${s3_key} `, error);
 
     return NextResponse.json(
       {
         status: 500,
-        message: `There was a problem deleting the file with s3_key ${params.s3_key}. Try again later.`
+        message: `There was a problem deleting the file with s3_key ${s3_key}. Try again later.`
       },
       { status: 500 }
     );
