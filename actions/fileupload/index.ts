23c4d807575ce2aa10794ad6e5206b437404f06d
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-07-20 18:31:49 -0700

    Remove unused handleFileDocUpdate Server Action

--- actions/fileupload/index.ts
+++ actions/fileupload/index.ts
@@ -2,13 +2,11 @@
 import { createFileDocuments } from './createFileDocuments';
 import { createNoteDocument } from './createNoteDocument';
 import { handleFileDeletion } from './handleFileDeletion';
-import { handleFileDocUpdate } from './handleFileDocUpdate';
 import { handlePresignedUrls } from './handlePresignedUrls';
 
 export {
   createFileDocuments,
   createNoteDocument,
   handleFileDeletion,
-  handleFileDocUpdate,
   handlePresignedUrls
 };
