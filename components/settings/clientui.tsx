32c0df701ce59ffb12f976c3ab33ae962cf055ff
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-07-29 16:49:50 -0700

    Render Account card in /settings page

--- components/settings/clientui.tsx
+++ components/settings/clientui.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { ReactNode } from 'react';
-
+import dayjs from 'dayjs';
+import { Avatar, Card, CardBody, CardHeader } from '@heroui/react';
 import { LeanUser } from '@/utils/mongodb';
 
 interface ClientUIProps {
@@ -9,10 +10,35 @@ interface ClientUIProps {
 
 export const ClientUI = ({ currentUser }: ClientUIProps): ReactNode => {
   console.log('currentUser in Settings Page ', currentUser);
+  const { first_name, last_name, email, avatar_url, sign_up_date } =
+    currentUser;
+  const fullName = `${first_name} ${last_name}`;
+  const avatarSrc =
+    avatar_url === null || avatar_url === '' ? undefined : avatar_url;
+
+  const dateJoined = dayjs(sign_up_date).format('M/D/YYYY');
 
   return (
-    <div>
-      <h1>⚙️ Settings Page</h1>
+    <div className="p-2">
+      <h1 className="text-3xl lg:text-6xl mb-16">⚙️ Settings Page</h1>
+
+      <Card className="max-w-[300px]">
+        <CardHeader>
+          <Avatar src={avatarSrc} />
+        </CardHeader>
+        <CardBody>
+          <h2 className="text-xl font-semibold mb-2">Account</h2>
+          <p className="mb-2">
+            <span className="font-semibold">Email</span>: {email}
+          </p>
+          <p className="mb-2">
+            <span className="font-semibold">Name</span>: {fullName}
+          </p>
+          <p>
+            <span className="font-semibold">Date Joined</span>: {dateJoined}
+          </p>
+        </CardBody>
+      </Card>
     </div>
   );
 };
