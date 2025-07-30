d9142f6870f6831e0e95414c6997e0fcfaaf386c
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-07-29 18:38:21 -0700

    Add User.cancel_date check in /signin-check
    If the user already deleted their account, we notify them
    they have to register using a different email or Social
    Auth account.

--- app/signin-check/page.tsx
+++ app/signin-check/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { ReactNode, useEffect } from 'react';
+import toast from 'react-hot-toast';
 import { useRouter } from 'next/navigation';
 import { Spinner } from '@heroui/react';
 
@@ -7,6 +8,8 @@ import { getUserFromDB } from '@/actions';
 
 const errorMessage =
   'Looks like you might have never registered to AlwaysSaved. 😬 Try again later.';
+
+const toastOptions = { duration: 6000 };
 
 export default function SignInCheckPage(): ReactNode {
   const router = useRouter();
@@ -18,6 +21,16 @@ export default function SignInCheckPage(): ReactNode {
       console.log('currentUser in /signin-check ', currentUser);
 
       if (currentUser) {
+        if (currentUser.cancel_date !== null) {
+          toast.error(
+            "It looks like you've deleted your account to AlwaysSaved. Register using a new email or Social Login.",
+            toastOptions
+          );
+          setTimeout(() => {
+            router.push('/');
+          }, toastOptions.duration);
+          return;
+        }
         router.push('/home');
       }
 
