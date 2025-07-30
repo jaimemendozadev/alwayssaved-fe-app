62d3ffd72a2c84cd21354603b882aaad6b443a44
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-07-29 17:31:47 -0700

    Scaffold deleteUserAccount Server Action

---
+++ actions/schemamodels/users.ts
@@ -0,0 +1,29 @@
+'use server';
+
+import {
+  deepLean,
+  getObjectIDFromString,
+  LeanUser,
+  UserModel
+} from '@/utils/mongodb';
+
+export const deleteUserAccount = async (userID: string): Promise<LeanUser> => {
+  const targetID = getObjectIDFromString(userID);
+  const deletedUser = await UserModel.findByIdAndUpdate(
+    targetID,
+    { cancel_date: new Date() },
+    {
+      returnDocument: 'after'
+    }
+  ).exec();
+
+  if (!deletedUser || deletedUser.date_deleted === null) {
+    throw new Error(
+      `There was a problem deleting the account for User ${userID}. Try again later.`
+    );
+  }
+
+  console.log('deletedUser ', deletedUser);
+
+  return deepLean(deletedUser);
+};
