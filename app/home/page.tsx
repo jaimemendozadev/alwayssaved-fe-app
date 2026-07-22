0cffe9edd5ec56999c4ee2f62a11c58cf7955c4e
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2026-07-22 04:59:56 -0700

    Update Media Upload Instructions text

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -43,22 +43,27 @@ export default function HomePage() {
         </p>
         <ol className="list-decimal ml-6">
           <li className="text-lg mb-8">
-            Fill out the form below to create a new Note by giving the note a new name.
+            Create a new Note by filling out the form to give it a new name.
           </li>
 
           <li className="text-lg mb-8">
-            Add video or audio files to your note for transcribing. <span className="font-bold">WAIT UNTIL ALL FILES ARE UPLOADED</span> to the cloud for transcribing.
+            Add video or audio files to your note for transcribing. <span className="font-bold">WAIT UNTIL ALL FILES ARE UPLOADED</span> to the cloud ☁️ for transcribing.
           </li>
           <ul className="text-lg mb-8">
-            <li className="mb-3 font-bold text-red-700">🙅🏽‍♀️ DO NOT GO TO ANOTHER PAGE IN THE APP WHILE UPLOADING FILES.</li>
+            <li className="font-bold text-red-700">🙅🏽‍♀️ DO NOT GO TO ANOTHER PAGE IN THE APP WHILE UPLOADING FILES.</li>
           </ul>
 
           <li className="text-lg mb-8">
-            When the files are finished uploading, the transcription process starts. You may now create a brand new Note with new media files or navigate to another part of the app.
+            When the files finish uploading, you&apos;re essentially done. You may now:
           </li>
 
+          <ul className="text-lg mb-8">
+            <li className="mb-3">Create a brand new Note with new media files; or</li>
+            <li>Navigate to another part of the app.</li>
+          </ul>
+
           <li className="text-lg mb-8">
-            We&apos;ll send you an email 📬 to let you know when your files are transcribed.
+            We&apos;ll send you an email 📬 to let you know when your files are finished processing so you conversate with the LLM about them. 🤖
           </li>
         </ol>
       </article>
