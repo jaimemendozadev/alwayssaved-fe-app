6edbe46fb77b34611df4c364e064cc87a7535270
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2026-07-25 09:35:06 -0700

    Delete removefilessection and move code to parent page

--- components/notes/[noteID]/edit/clientui.tsx
+++ components/notes/[noteID]/edit/clientui.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import dayjs from 'dayjs';
@@ -12,13 +12,10 @@ import {
   LeanConversation
 } from '@/utils/mongodb';
 import { deleteNoteByID } from '@/actions/schemamodels/notes';
-import {
-  createConversation,
-  deleteConvoByID
-} from '@/actions/schemamodels/conversations';
+import { deleteConvoByID } from '@/actions/schemamodels/conversations';
 import { deleteMessagesByConvoID } from '@/actions/schemamodels/convomessages';
+import { purgeFileByID } from '@/actions/schemamodels/files';
 import { DeleteModal } from '@/components/deletemodal';
-import { RemoveFilesSection } from './components';
 import { UploadInstructions } from '@/components/uploadinstructions';
 import { FileUpload } from '@/components/fileupload';
 
@@ -41,6 +38,7 @@ export const ClientUI = ({
 }: ClientUIProps): ReactNode => {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
   const router = useRouter();
+  const [targetFile, setTargetFile] = useState<LeanFile | null>(null);
 
   const handleRedirect = () => {
     router.refresh();
@@ -64,6 +62,29 @@ export const ClientUI = ({
     );
 
     onClose();
+  };
+
+  const deleteTextFile = async (onClose: () => void) => {
+    if (targetFile === null) return;
+
+    const { _id, file_type } = targetFile;
+
+    const deletedFile = await purgeFileByID(_id, file_type);
+
+    if (deletedFile && deletedFile.date_deleted) {
+      toast.success(
+        'Your text transcript File was successfully deleted. 👍🏽',
+        toastOptions
+      );
+      onClose();
+      setTargetFile(null);
+      router.refresh();
+      return;
+    }
+
+    throw new Error(
+      `There was an error deleting the .txt File ${_id} for User ${currentUser._id}.`
+    );
   };
 
   // See Dev Note #2 below.
@@ -171,9 +192,91 @@ export const ClientUI = ({
 
       <hr className="mb-16" />
 
+      {/*  Remove Files Attached to Your Note */}
+
+      <h2 className="text-3xl lg:text-4xl mb-10">
+        Remove Files Attached to Your Note
+      </h2>
+
+      <article className="mb-24">
+        <p className="text-lg mb-5 font-bold text-red-700">
+          📢 WARNING ABOUT DELETING .txt FILES:
+        </p>
+
+        <p className="text-lg mb-3">
+          If you delete a .txt text file from your Note, any Conversation that
+          references that text File{' '}
+          <strong>
+            will severely affect the quality of your ongoing Conversation
+          </strong>
+          .
+        </p>
+
+        <p className="text-lg">
+          In other words, the LLM will no longer understand what you&apos;re
+          talking about going forward. 😵‍💫
+        </p>
+      </article>
+
+      <DeleteModal
+        deleteCallback={deleteTextFile}
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        resourceType=".txt File"
+      />
+
+      {noteFiles.length > 0 && (
+        <ul className="space-y-4 mb-16">
+          {noteFiles.map((fileDoc) => (
+            <li key={fileDoc._id} className="border p-5">
+              <span className="font-semibold">File Name</span>:{' '}
+              {fileDoc.file_name} &nbsp; | &nbsp;{' '}
+              <span className="font-semibold">File Type</span>:{' '}
+              {fileDoc.file_type} &nbsp; | &nbsp;{' '}
+              <Tooltip content="Delete File">
+                <Button
+                  size="sm"
+                  variant="ghost"
+                  isIconOnly={true}
+                  aria-label="Delete"
+                  onPress={async () => {
+                    if (fileDoc.file_type === '.txt') {
+                      setTargetFile(fileDoc);
+                      onOpen();
+                      return;
+                    }
+
+                    const purgedResult = await purgeFileByID(
+                      fileDoc._id,
+                      fileDoc.file_type
+                    );
+
+                    if (purgedResult.date_deleted) {
+                      toast.success(
+                        'Your File has been deleted. 👍🏽',
+                        toastOptions
+                      );
+                      router.refresh();
+                    }
+                  }}
+                >
+                  🗑️
+                </Button>
+              </Tooltip>
+            </li>
+          ))}
+        </ul>
+      )}
+
+      <hr className="mb-16" />
+
+      {/* Upload More Files */}
+
       <h2 className="text-3xl lg:text-4xl mb-10">
         💿 Upload More Files to Your Note
       </h2>
+
+      {/* TODO: Will have to craft a new set of instructions for uploadidng files to an existing Note. */}
 
       <UploadInstructions />
 
@@ -184,8 +287,6 @@ export const ClientUI = ({
           routerCallback={handleRedirect}
         />
       </div>
-
-      <RemoveFilesSection currentUser={currentUser} noteFiles={noteFiles} />
     </div>
   );
 };
--- components/notes/[noteID]/edit/components/index.ts
+++
@@ -1,2 +0,0 @@
-import { RemoveFilesSection } from './removefilessection';
-export { RemoveFilesSection };
