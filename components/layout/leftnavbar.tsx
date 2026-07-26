1520f97f4af84cdea17234cbc9392adc2c41b0de
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2026-07-26 16:31:40 -0700

    Change LeftNavBar from <div>s to <ul> list

--- components/layout/leftnavbar.tsx
+++ components/layout/leftnavbar.tsx
@@ -4,36 +4,36 @@ import { SignOutButton } from '@clerk/nextjs';
 
 export const LeftNavbar = (): ReactNode => {
   return (
-    <div className="max-w-1/6 min-h-screen border bg-[#f9f9f9] p-4">
-      <div className="min-w-28 p-5 mb-2 w-36">
+    <ul className="max-w-1/6 min-h-screen border bg-[#f9f9f9] p-4">
+      <li className="min-w-28 p-5 mb-2 w-36">
         <p className="text-[#919191] font-semibold">
           <Link href="/home">🏡 Home</Link>
         </p>
-      </div>
+      </li>
 
-      <div className="min-w-28 p-5 mb-2 w-36">
+      <li className="min-w-28 p-5 mb-2 w-36">
         <p className="text-[#919191] font-semibold">
           <Link href="/notes">📝 Notes</Link>
         </p>
-      </div>
+      </li>
 
-      <div className="min-w-28 p-5 mb-2 w-36">
+      <li className="min-w-28 p-5 mb-2 w-36">
         <p className="text-[#919191] font-semibold">
           <Link href="/convos">💬 Convos</Link>
         </p>
-      </div>
+      </li>
 
-      <div className="min-w-28 p-5 mb-2 w-36">
+      <li className="min-w-28 p-5 mb-2 w-36">
         <p className="text-[#919191] font-semibold">
           <Link href="/settings">⚙️ Settings</Link>
         </p>
-      </div>
+      </li>
 
-      <div className="min-w-28 p-5 w-36">
+      <li className="min-w-28 p-5 w-36">
         <div className="text-[#919191] font-semibold">
-          <SignOutButton redirectUrl='/' />
+          🚪 <SignOutButton redirectUrl="/" />
         </div>
-      </div>
-    </div>
+      </li>
+    </ul>
   );
 };
